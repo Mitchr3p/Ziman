a3ff01921776971a3c859ce815c27204d3aecb4e
Mitchell <mitchellr3p@gmail.com>
2024-01-06 20:31:53 +0100

    add code

--- app.js
+++ app.js
@@ -5,22 +5,6 @@ toggleBtn.addEventListener("click", function () {
   dropdownMenu.classList.toggle("open");
 });
 
-// let sectioneers = document.querySelectorAll('.sectioneer');
-
-// window.onscroll = () => {
-//   sectioneers.forEach(sectioneer => {
-//     let top = window.scrollY;
-//     let offset = sectioneer.offsetTop - 150;
-//     let height = sectioneer.offsetHeight;
-
-//     if (top >= offset && top < offset + height) {
-//       sectioneer.classList.add('show-animate');
-//     } else {
-//       sectioneer.classList.remove('show-animate');
-//     }
-//   });
-// };
-
 let sectioneers = document.querySelectorAll('.sectioneer');
 const changeHer = document.querySelector(".hero")
 
@@ -45,13 +29,6 @@ window.onscroll = () => {
   const togleanime = document.querySelector(".animated-text2")
   const changeNav = document.querySelector(".navigation")
 
-  // EITHER DO IT DIRECTLY WITH .style
-  // if (sec2.classList.contains('show-animate')) {
-  //   sec1.style.background = "burlywood"
-  // } else {
-  //   sec1.style.background = "#1f242d"
-  // }
-
   //OR ADD A NEW CLASS AND STYLE IT IN THE CSS ->BETTER
   if (sec2.classList.contains('show-animate')) {
     sec1.classList.add('bg-change');
@@ -71,37 +48,22 @@ window.onscroll = () => {
 
 changeHer.addEventListener('click', function() {
   window.location = 'index.html'
-})
-
-let divioneer = document.querySelector(".thru");
-let toShow1 = document.querySelector(".ethos");
-let toShow2= document.querySelector(".values");
-let toShow3 = document.querySelector(".approach");
-
-window.onscroll = () => {
-  divioneer.forEach(divioneer => {
-    let top = window.scrollY;
-    let offset = divioneer.offsetTop - 600;
-    let height = divioneer.offsetHeight;
-
-    if (top >= offset && top < offset + height) {
-      toShow1.style.display = "none";
-    } else {
-      toShow1.style.display = 'block'
-    }
-  })
-}
-
-const popCnt = document.querySelector('.popupContent');
-const popBtn = document.querySelector('.popupButton');
-const popClose = document.querySelector('.closeButton');
-
-popBtn.addEventListener('click', function() {
-  popCnt.classList.add('popon');
 });
 
-popClose.addEventListener('click', function() {
-  popCnt.classList.remove('popon');
+const popCnt = document.querySelectorAll('.popupContent');
+const popBtn31 = document.querySelectorAll('.popupButton');
+const popClose = document.querySelectorAll('.closeButton');
+
+popBtn31.forEach((btn, index) => {
+  btn.addEventListener('click', function() {
+    popCnt[index].classList.add('popon');
+  });
+});
+
+popClose.forEach((btn, index) => {
+  btn.addEventListener('click', function() {
+    popCnt[index].classList.remove('popon');
+  });
 });
 
 // Enter key: 13
@@ -115,11 +77,44 @@ popClose.addEventListener('click', function() {
 // Control key: 17
 // Alt key: 18
 
-// const inputElement = document.getElementById('myInput');
-// const outputElement = document.getElementById('output');
+const inputElement1 = document.getElementById('myInput1');
+const inputElement2 = document.getElementById('myInput2');
+const inputElement3 = document.getElementById('myInput3');
+const outputElement1 = document.getElementById('output-js1');
+const outputElement2 = document.getElementById('output-js2');
+const outputElement3 = document.getElementById('output-js3');
+const finalBtn = document.getElementById('finalize');
+const finalTxt = document.querySelector(".final-text");
 
-// inputElement.oninput = function() {
-//   // Get the input value and display it in the output paragraph
-//   const inputValue = inputElement.value;
-//   outputElement.textContent = `You typed: ${inputValue}`;
-// };
+inputElement1.oninput = function() {
+  // Get the input value and display it in the output paragraph
+  let inputValue1 = inputElement1.value;
+  outputElement1.textContent = `Hi ${inputValue1}`;
+};
+
+inputElement2.oninput = function() {
+  let inputValue2 = inputElement2.value;
+  outputElement2.textContent = `You are ${inputValue2} years old`;
+};
+
+inputElement3.oninput = function() {
+  let inputValue3 = inputElement3.value;
+  outputElement3.textContent = `- ${inputValue3}`;
+};
+
+finalBtn.addEventListener('click', function() {
+  finalTxt.textContent = ` Thanks for the Message ${inputElement1.value} `
+  finalTxt.classList.add('finpop')
+
+  inputElement1.value = ''
+  inputElement2.value = ''
+  inputElement3.value = ''
+  outputElement1.textContent = ''
+  outputElement2.textContent = ''
+  outputElement3.textContent = '';
+
+
+  setTimeout(function() {
+    finalTxt.classList.remove('finpop')
+  }, 3000);
+})
